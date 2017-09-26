f29ea8cec5d839d5bacd96553e92b237282c7bc6
Keith Maggio <utman1988@gmail.com>
2017-09-26 10:33:27 -0400

    Removed commented-out paths

--- makeSounds.bat
+++ makeSounds.bat
@@ -1,5 +1,3 @@
-REM SET comp="C:\Users\keith ctr maggio\Documents\Arduino\libraries\ArdBitmap\extras\compressor\compressor2.0.jar"
-REM SET java="C:\Program Files\Java\jdk1.8.0_71\bin\java.exe"
 SET comp="C:\Users\utman\Documents\Arduino\libraries\ArdVoice\extras\vocoder\vocoder0.2.jar"
 SET java="C:\Program Files (x86)\Java\jre1.8.0_144\bin\java.exe"
 
